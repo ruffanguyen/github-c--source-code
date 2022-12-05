3aba85218a2aa09c622764f2dae43b226bcbb3ad
LK Ruffa <ruffanguyen.dev@gmail.com>
2022-12-05 17:04:12 +0700

    fig02_14.cpp

--- fig02_14.cpp
+++ fig02_14.cpp
@@ -1,25 +1,18 @@
-// Fig. 2.14: fig02_14.cpp
-// Preincrementing and postincrementing.
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-using std::cout;
-using std::endl;
-
-// function main begins program execution
 int main() {
-    int c;   // declare variable
+    int c;                  // declare variable
     // demonstrate postincrement
-    c = 5;   // assign 5 to c
-    cout << c << endl;   // print 5
-    cout << c++ << endl; // print 5 then postincrement
-    cout << c << endl << endl; // print 6 
+    c = 5;                  // assign 5 to c
+    cout << c << endl;      // print 5
+    cout << c++ << endl;    // print 5 then postincrement
+    cout << c << endl;      // print 6
+    //demonstrate preincrement
+    c = 5;                  // assign 5 to c
+    cout << c << endl;      // print 5
+    cout << ++c << endl;    // preincrement then print 6
+    cout << c << endl;      // print 6
 
-    // demonstrate preincrement
-    c = 5;   // assign 5 to c
-    cout << c << endl;   // print 5
-    cout << ++c << endl; // preincrement then print 6 
-    cout << c << endl;   // print 6 
-
-    return 0;   // indicate successful termination
-
-} // end function main
+    return 0;
+}
